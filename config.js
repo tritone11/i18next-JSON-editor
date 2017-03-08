c5db7aa510a0b1eb702498c53c8ed5f3a1c144da
Tritone <gioele@officinetesla.it>
2017-03-08 17:33:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+//////////////	Configuration files of ies18n
+//////////////	Author: tRitone11 - ies italia
+window.defaultLanguage='it';//Lingua di default per le select
+window.successColor='green';//color of modal success
+window.errorColor='red';//color of modal error
+window.defaultColor='black';//color of modal default
+
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+var config = {
+    apiKey: "AIzaSyALgbJGJY_zQKvD-dQ8o7sazV1VMySXB5w",
+    authDomain: "languages-seafy.firebaseapp.com",
+    databaseURL: "https://languages-seafy.firebaseio.com",
+    storageBucket: "languages-seafy.appspot.com",
+    messagingSenderId: "120296278792"
+  };
+  firebase.initializeApp(config);
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
+//////////////	FIREBASE SNIPPET !!!!!!!IMPORTANT: PASTE ONLY THE SCRIPT, NOT THE HTML TAGS <SCRIPT>!!!!!!
